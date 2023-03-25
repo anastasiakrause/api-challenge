8b33a0f3f3f3972016f5673c347218cf55f77e70
Anastasia Krause <anastasiakrause@Anastasias-MacBook-Pro.local>
2023-03-25 21:05:23 +0000

    Fix timestamp validator

--- backend/app/api/helpers.py
+++ backend/app/api/helpers.py
@@ -47,7 +47,7 @@ def validate_timestamp_iso(timestamp):
     pattern = r'^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(\.[0-9]+)?(Z|[+-](?:2[0-3]|[01][0-9]):[0-5][0-9])?$'
     match_iso8601 = re.compile(pattern).match
     try:            
-        if match_iso8601( str_val ) is not None:
+        if match_iso8601( timestamp ) is not None:
             return True
     except:
         pass
